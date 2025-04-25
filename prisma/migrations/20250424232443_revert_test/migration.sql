4ba79948bc22911cf2d1a4fd92cf86276a125fc0
nekoo7 <abylaiooaooa@mail.ru>
2025-04-25 05:04:22 +0500

    prisma conn

---
+++ prisma/migrations/20250424232443_revert_test/migration.sql
@@ -0,0 +1,16 @@
+/* ---------- 1. Удаляем внешние ключи ---------- */
+ALTER TABLE "RefreshToken" DROP CONSTRAINT IF EXISTS "RefreshToken_userId_fkey";
+ALTER TABLE "Post"         DROP CONSTRAINT IF EXISTS "Post_authorId_fkey";
+
+/* ---------- 2. Удаляем уникальные индексы ---------- */
+DROP INDEX IF EXISTS "RefreshToken_token_key";
+DROP INDEX IF EXISTS "User_email_key";
+DROP INDEX IF EXISTS "Post_slug_key";
+
+/* ---------- 3. Удаляем таблицы ---------- */
+DROP TABLE IF EXISTS "RefreshToken";
+DROP TABLE IF EXISTS "Post";
+DROP TABLE IF EXISTS "User";
+
+/* ---------- 4. Удаляем enum-тип ---------- */
+DROP TYPE IF EXISTS "Role";
---
+++ prisma/migrations/20250424233209_/migration.sql
@@ -0,0 +1,54 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('author', 'reader');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'reader',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "revoked" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "coverUrl" TEXT,
+    "excerpt" TEXT,
+    "contentMarkdown" TEXT NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_slug_key" ON "Post"("slug");
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
